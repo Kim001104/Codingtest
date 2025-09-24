087822facbf04d5d91058f6726987a51252bea67
Kim001104 <dongsky2000@naver.com>
2025-09-24 21:55:02 +0900

    1157번 해결

--- Implementation/src/1157.c
+++ Implementation/src/1157.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int a;
-    scanf("%d", &a);
-    printf("%d", a);
+
+    char str[1000001];  //입력 버퍼는 백준에 명시되어있는대로 해야 함. 
+    scanf("%s", str);                 
+    int len = strlen(str);
+
+    char alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";   // \0값 포함 27
+    int cnt[26] = {0, };    // cnt 배열 초기화
+
+    // 소문자 -> 대문자
+    for (int i = 0; i < len; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') str[i] -= 32;
+    }
+
+    // 알파벳 개수 세기
+    for (int i = 0; i < len; i++) {
+        // alphabet[i]와 비교하지 말고 A~Z 범위면 해당 인덱스 증가
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            cnt[str[i] - 'A']++;
+        }
+    }
+
+    // 최대값 출력
+    int max = 0;
+    char tmp = '?';
+    for (int i = 0; i < 26; i++) {
+        if (cnt[i] > max) {
+            max = cnt[i];
+            tmp = alphabet[i];
+        } else if (cnt[i] == max && max != 0) {
+            tmp = '?';
+        }
+    }
+
+    printf("%c\n", tmp);
     return 0;
 }
